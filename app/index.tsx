3d79bb747fec323014db5abeccfdd14411abc180
nk0905 <0dd2n.a0905@gmail.com>
2024-09-10 22:28:38 +0900

    add appgradient component and tabs directory

--- app/index.tsx
+++ app/index.tsx
@@ -4,8 +4,11 @@ import beachImage from '@/assets/meditation-images/beach.webp';
 import { LinearGradient } from 'expo-linear-gradient';
 import { StatusBar } from 'expo-status-bar';
 import CustomButton from '@/components/CustomButton';
+import { useRouter } from 'expo-router';
+import AppGradient from '@/components/AppGradient';
 
 const App = () => {
+  const router = useRouter();
   return (
     <View className="flex-1">
       <ImageBackground
@@ -13,11 +16,8 @@ const App = () => {
         resizeMode="cover"
         className="flex-1"
       >
-        <LinearGradient
-          className="flex-1"
-          colors={['rgba(0, 0, 0, 0.4)', 'rgba(0, 0, 0, 0.8)']}
-        >
-          <SafeAreaView className="flex-1 mx-5 my-12 justify-between">
+        <AppGradient colors={['rgba(0, 0, 0, 0.4)', 'rgba(0, 0, 0, 0.8)']}>
+          <SafeAreaView className="flex-1 px-1 justify-between">
             <View>
               <Text className="text-center text-white font-bold text-4xl">
                 Simple Meditation
@@ -28,13 +28,13 @@ const App = () => {
             </View>
             <View>
               <CustomButton
-                onPress={() => console.log('tap')}
+                onPress={() => router.push('/nature-meditate')}
                 title="Get Started"
               />
             </View>
             <StatusBar style="light" />
           </SafeAreaView>
-        </LinearGradient>
+        </AppGradient>
       </ImageBackground>
     </View>
   );
