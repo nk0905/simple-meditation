112ad7cb2f711281b302ed331eef9f3c35f79b1d
nk0905 <0dd2n.a0905@gmail.com>
2024-09-11 22:53:52 +0900

    add style meditation index screen

--- app/(tabs)/nature-meditate.tsx
+++ app/(tabs)/nature-meditate.tsx
@@ -1,10 +1,23 @@
 import { View, Text } from 'react-native';
 import React from 'react';
+import AppGradient from '@/components/AppGradient';
+import { StatusBar } from 'expo-status-bar';
 
 const NatureMeditate = () => {
   return (
-    <View>
-      <Text>NatureMeditate</Text>
+    <View className="flex-1">
+      <AppGradient colors={['#161b2e', '#0a4d4a', '#766e67']}>
+        <View className="mb-6">
+          <Text className="text-gray-200 mb-3 font-bold text-4xl text-left">
+            Welcome Steven
+          </Text>
+          <Text className="text-indigo-100 text-xl font-medium">
+            Start your meditation practice today
+          </Text>
+        </View>
+      </AppGradient>
+
+      <StatusBar style="light" />
     </View>
   );
 };
