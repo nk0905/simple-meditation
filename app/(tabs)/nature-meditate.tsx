c8af0b7c83bfb85b7d6a3004fc1734a33164f07a
nk0905 <0dd2n.a0905@gmail.com>
2024-09-12 23:20:18 +0900

    add flatlist

--- app/(tabs)/nature-meditate.tsx
+++ app/(tabs)/nature-meditate.tsx
@@ -1,7 +1,9 @@
-import { View, Text } from 'react-native';
+import { View, Text, FlatList, Pressable, ImageBackground } from 'react-native';
 import React from 'react';
 import AppGradient from '@/components/AppGradient';
 import { StatusBar } from 'expo-status-bar';
+import { MEDITATION_DATA } from '@/constants/MeditationData';
+import MEDITATION_IMAGES from '@/constants/meditation-images';
 
 const NatureMeditate = () => {
   return (
@@ -14,6 +16,30 @@ const NatureMeditate = () => {
           <Text className="text-indigo-100 text-xl font-medium">
             Start your meditation practice today
           </Text>
+        </View>
+        <View>
+          <FlatList
+            data={MEDITATION_DATA}
+            className="mb-20"
+            keyExtractor={(item) => item.id.toString()}
+            showsVerticalScrollIndicator={false}
+            renderItem={({ item }) => (
+              <Pressable
+                onPress={() => console.log('press')}
+                className="h-48 my-3 rounded-md overflow-hidden"
+              >
+                <ImageBackground
+                  source={MEDITATION_IMAGES[item.id - 1]}
+                  resizeMode="cover"
+                  className="flex-1 rounded-lg justify-center"
+                >
+                  <Text className="text-gray-100 text-3xl font-bold text-center">
+                    {item.title}
+                  </Text>
+                </ImageBackground>
+              </Pressable>
+            )}
+          />
         </View>
       </AppGradient>
 
