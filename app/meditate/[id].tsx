f5a414ed335bec723055a41add97ebf1c479c87b
nk0905 <0dd2n.a0905@gmail.com>
2024-09-15 22:47:22 +0900

    add meditate page

---
+++ app/meditate/[id].tsx
@@ -0,0 +1,30 @@
+import { View, Text, ImageBackground, Pressable } from 'react-native';
+import React from 'react';
+import MEDITATION_IMAGES from '@/constants/meditation-images';
+import AppGradient from '@/components/AppGradient';
+import AntDesign from '@expo/vector-icons/AntDesign';
+import { router, useLocalSearchParams } from 'expo-router';
+
+const Meditate = () => {
+  const { id } = useLocalSearchParams();
+  return (
+    <View className="flex-1">
+      <ImageBackground
+        source={MEDITATION_IMAGES[Number(id) - 1]}
+        resizeMode="cover"
+        className="flex-1"
+      >
+        <AppGradient colors={['transparent', 'rgba(0 ,0, 0, 0.8)']}>
+          <Pressable
+            onPress={() => router.back()}
+            className="absolute top-16 left-6 z-10"
+          >
+            <AntDesign name="leftcircleo" size={50} color="white" />
+          </Pressable>
+        </AppGradient>
+      </ImageBackground>
+    </View>
+  );
+};
+
+export default Meditate;
